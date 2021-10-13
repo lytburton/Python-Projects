27fae7b67d9989c161419d51ce253ca2b0555cbe
Lyttia Burton <lytburton@gmail.com>
2021-10-12 23:38:57 -0500

    Added inheritance assignment

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+#created a parent class of Person with 3 attributes
+class Person:
+    height = 'no height provided'
+    weight = '123 lbs'
+    age = 0
+
+#created a child class 'Man' with 2 attributes
+class Man(Person):
+    has_beard = True
+    has_mustache = False
+
+#created a child class 'Woman' with 2 attributes
+class Woman(Person):
+    wears_makeup = True
+    favorite_nail_polish = ' '
+
+
+    
+    
